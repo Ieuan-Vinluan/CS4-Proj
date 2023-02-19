276dc957e8cc349e6f9a5a9779d305296250b1e6
Ieuan-Vinluan <b2025idrvinluan@pshs.edu.ph>
2023-02-19 19:40:16 +0800

    now allows the user to go from home to subjects

--- application/HomeScreenController.java
+++ application/HomeScreenController.java
@@ -1,34 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
 
-/**
- *
- * @author TRUTH
- */
 public class HomeScreenController {
-    
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private Button subject;
+
+    @FXML
+    void goToSubject(ActionEvent event) throws IOException {
+
+        // getting current window
+        Node node = (Node) event.getSource();
+        Scene currentScene = node.getScene();
+        Stage currentStage = (Stage) currentScene.getWindow();
+
+        // loading new window
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/subject.fxml"));
+        Parent root = loader.load();
+        Scene newScene = new Scene(root);
+
+        currentStage.hide();
+        currentStage.setScene(newScene);
+        currentStage.show();
+
+        SubjectController sc = loader.getController();
+        System.out.println("Successfully opened Subjects"); // debug purposes
     }
 
-    public void initialize() {
-        // TODO
-    }    
-    
+    @FXML
+    void initialize() {
+
+    }
+
 }
