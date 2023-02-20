5dff536792987cf4727ce13d7ad4a6395f6b3481
Ieuan-Vinluan <b2025idrvinluan@pshs.edu.ph>
2023-02-20 14:55:49 +0800

    updated layout of subject.fxml

--- application/SubjectController.java
+++ application/SubjectController.java
@@ -1,9 +1,16 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class SubjectController {
 
@@ -13,12 +20,32 @@ public class SubjectController {
     @FXML
     private URL location;
 
+    @FXML
+    private Button subject;
 
     @FXML
-    private ImageView appLogo;
+    void goToSubject(ActionEvent event) throws IOException {
+        // getting current window
+        Node node = (Node) event.getSource();
+        Scene currentScene = node.getScene();
+        Stage currentStage = (Stage) currentScene.getWindow();
+
+        // loading new window
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/subject.fxml"));
+        Parent root = loader.load();
+        Scene newScene = new Scene(root);
+
+        currentStage.hide();
+        currentStage.setScene(newScene);
+        currentStage.show();
+
+        SubjectController sc = loader.getController();
+        System.out.println("Successfully opened Subjects"); // debug purposes
+    }
 
     @FXML
     void initialize() {
 
     }
+
 }
