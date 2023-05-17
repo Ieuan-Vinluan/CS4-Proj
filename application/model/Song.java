bc147f43fa9a8838a559e7198748a5f13a60bac4
Ieuan-Vinluan <b2025idrvinluan@pshs.edu.ph>
2023-05-18 03:24:00 +0800

    Finished documentation

--- application/model/Song.java
+++ application/model/Song.java
@@ -36,10 +36,9 @@ public class Song {
     private static ArrayList<Song> songs = new ArrayList<>();
 
     /**
-     *
-     * @param title the title of the Song
-     * @param artist the name of the artist who made the song
-     * @param added the date on which the Song was added to the music player
+     * @param title    the title of the Song
+     * @param artist   the name of the artist who made the song
+     * @param added    the date on which the Song was added to the music player
      * @param playlist the Playlist that contains the Song
      * @param filePath the file path to the Song's mp3 file
      */
@@ -59,6 +58,7 @@ public class Song {
 
     /**
      * Returns the title of the Song
+     *
      * @return the title of the Song
      */
     public String getTitle() {
@@ -67,6 +67,7 @@ public class Song {
 
     /**
      * Sets the title of the Song
+     *
      * @param title the new title of the Song
      */
     public void setTitle(String title) {
@@ -75,6 +76,7 @@ public class Song {
 
     /**
      * Returns the Playlist that contains the Song
+     *
      * @return the Playlist that contains the Song
      */
     public Playlist getPlaylist() {
@@ -83,6 +85,7 @@ public class Song {
 
     /**
      * Sets the Playlist that contains the Song
+     *
      * @param playlist the new Playlist that will contain the Song
      */
     public void setPlaylist(Playlist playlist) {
@@ -91,6 +94,7 @@ public class Song {
 
     /**
      * Returns the name of the artist who made the Song
+     *
      * @return the name of the artist who made the Song
      */
     public String getArtist() {
@@ -99,6 +103,7 @@ public class Song {
 
     /**
      * Sets the name of the artist who made the song
+     *
      * @param artist the name of the artist who made the song
      */
     public void setArtist(String artist) {
@@ -107,6 +112,7 @@ public class Song {
 
     /**
      * Returns the file path of the Song's mp3 file
+     *
      * @return the file path of the Song's mp3 file
      */
     public String getFilePath() {
@@ -115,6 +121,7 @@ public class Song {
 
     /**
      * Sets the file path that points to the Song's mp3 file
+     *
      * @param filePath the new file path that will point to the Song's mp3 file
      */
     public void setFilePath(String filePath) {
@@ -123,6 +130,7 @@ public class Song {
 
     /**
      * Returns the date on which the Song was added to the music player
+     *
      * @return the date on which the Song was added to the music player
      */
     public String getAdded() {
@@ -131,28 +139,52 @@ public class Song {
 
     /**
      * Sets the date on which the Song was added to the music player
+     *
      * @param added the date on which the Song was added to the music player
      */
     public void setAdded(String added) {
         this.added = added;
     }
 
+    /**
+     * Returns the boolean value that indicates whether the Song is playing or not
+     *
+     * @return the boolean value that indicates whether the Song is playing or not
+     */
     public boolean getPlaying() {
         return isPlaying;
     }
 
+    /**
+     * Sets the boolean value that indicates whether the Song is playing or not
+     *
+     * @param isPlaying the boolean value that indicates whether the Song is playing or not
+     */
     public void setPlaying(boolean isPlaying) {
         this.isPlaying = isPlaying;
     }
 
+    /**
+     * Returns the ArrayList of all created Songs
+     *
+     * @return the ArrayList of all created Songs
+     */
     public static ArrayList<Song> getSongs() {
         return songs;
     }
 
+    /**
+     * Sets the ArrayList of all created Songs to another ArrayList containing Songs
+     *
+     * @param songs an ArrayList containing Songs
+     */
     public static void setSongs(ArrayList<Song> songs) {
         Song.songs = songs;
     }
 
+    /**
+     * Toggles the "playing state" of the Song. If it is playing, then the Song is paused (i.e., the 'isPlaying' field is set to false). If it is not playing, then the Song is played (i.e., the 'isPlaying' field is set to true).
+     */
     public void toggleSong() {
         this.isPlaying = !this.isPlaying;
     }
--- application/model/Subject.java
+++ application/model/Subject.java
@@ -2,73 +2,151 @@ package application.model;
 
 import java.util.ArrayList;
 
+/**
+ * The Subject class represents a "course" that a student would have to go through in school. It has a subject name, an ArrayList of LearningGuides that the student can access, an ArrayList of Quizzes that the student can answer, and an ArrayList of Notes that are related to that Subject. In addition to this, each Subject has an image that is used to represent that particular Subject all throughout the app.
+ */
 public class Subject {
+    /**
+     * The name of the Subject
+     */
     private String subjectName;
+    /**
+     * An ArrayList of LearningGuides that are related to this Subject
+     */
     private ArrayList<LearningGuide> learningGuides = new ArrayList<>();
+    /**
+     * An ArrayList of Quizzes that are related to this Subject
+     */
     private ArrayList<Quiz> quizzes = new ArrayList<>();
+    /**
+     * An ArrayList of Notes that are related to this Subject
+     */
     private ArrayList<Note> notes = new ArrayList<>();
+    /**
+     * The file name of the image in the "images" directory that represents this Subject
+     */
     private String imageFilename;
+    /**
+     * An ArrayList of all created Subjects
+     */
     private static ArrayList<Subject> subjects = new ArrayList<>();
 
+    /**
+     * @param subjectName   the name of the Subject
+     * @param imageFilename the file name of the image in the "images" directory that represents this Subject
+     */
     public Subject(String subjectName, String imageFilename) {
         this.subjectName = subjectName;
         this.imageFilename = imageFilename;
         subjects.add(this);
     }
 
+    /**
+     * Returns the file name of the image in the "images" directory that represents this Subject
+     *
+     * @return the file name of the image in the "images" directory that represents this Subject
+     */
     public String getImageFilename() {
         return imageFilename;
     }
 
+    /**
+     * Sets the file name of the image in the "images" directory that represents this Subject
+     *
+     * @param imageFilename the file name of the new image in the "images" directory that represents this Subject
+     */
     public void setImageFilename(String imageFilename) {
         this.imageFilename = imageFilename;
     }
 
+    /**
+     * Returns the ArrayList of all created Subjects
+     *
+     * @return the ArrayList of all created Subjects
+     */
     public static ArrayList<Subject> getSubjects() {
         return subjects;
     }
 
+    /**
+     * Sets the ArrayList of all created Subjects to another ArrayList that contains Subjects
+     *
+     * @param subjects another ArrayList that contains Subjects
+     */
     public static void setSubjects(ArrayList<Subject> subjects) {
         Subject.subjects = subjects;
     }
 
+    /**
+     * Returns the name of the Subject
+     *
+     * @return the name of the Subject
+     */
     public String getSubjectName() {
         return subjectName;
     }
 
+    /**
+     * Sets the name of the Subject
+     *
+     * @param subjectName the new name of the Subject
+     */
     public void setSubjectName(String subjectName) {
         this.subjectName = subjectName;
     }
 
+    /**
+     * Returns the ArrayList of Quizzes associated with this Subject
+     *
+     * @return the ArrayList of Quizzes associated with this Subject
+     */
     public ArrayList<Quiz> getQuizzes() {
         return quizzes;
     }
 
+    /**
+     * Sets the ArrayList of Quizzes associated with this Subject to another ArrayList containing Quizzes
+     *
+     * @param quizzes another ArrayList containing Quizzes
+     */
     public void setQuizzes(ArrayList<Quiz> quizzes) {
         this.quizzes = quizzes;
     }
 
+    /**
+     * Returns the ArrayList of Notes associated with this Subject
+     *
+     * @return the ArrayList of Notes associated with this Subject
+     */
     public ArrayList<Note> getNotes() {
         return notes;
     }
 
+    /**
+     * Sets the ArrayList of Notes associated with this Subject to another ArrayList containing Notes
+     *
+     * @param notes another ArrayList containing Notes
+     */
     public void setNotes(ArrayList<Note> notes) {
         this.notes = notes;
     }
 
+    /**
+     * Returns the ArrayList of LearningGuides associated with this Subject
+     *
+     * @return the ArrayList of LearningGuides associated with this Subject
+     */
     public ArrayList<LearningGuide> getLearningGuides() {
         return learningGuides;
     }
 
+    /**
+     * Sets the ArrayList of LearningGuides associated with this Subject to another ArrayList containing LearningGuides
+     *
+     * @param learningGuides another ArrayList containing LearningGuides
+     */
     public void setLearningGuides(ArrayList<LearningGuide> learningGuides) {
         this.learningGuides = learningGuides;
-    }
-
-    public void showContents() {
-        int index = 1;
-        for (LearningGuide lg : learningGuides) {
-            System.out.println(index + ". " + lg.getTitle());
-        }
     }
 
 }
